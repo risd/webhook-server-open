e6aa2fb93d5871b780cf573fdd4c82430ffca3e6
rubonics <rubenrodriguez+github@gmail.com>
2018-04-26 18:33:59 -0400

    updates backup & adds test
    + updates backup to use new firebase api
    + adds test for ensuring the backup process finishes without error
    
    currently the backup test does not pass. the access token that is produced does not have the correct permissions

--- libs/backup.js
+++ libs/backup.js
@@ -6,84 +6,179 @@
 * if we need to
 */
 
-var request = require('request');
-var cloudStorage = require('./cloudStorage.js');
-var firebase = require('firebase');
-var _ = require('lodash');
+var request = require( 'request' )
+var async = require( 'async' )
+var cloudStorage = require( './cloudStorage.js' )
+var Firebase = require( './firebase/index.js' )
+var _ = require( 'lodash' )
 
 
 /**
 * @params config The configuration from Grunt
 * @params logger Logger to use, deprecated, does not actually get used at all
 */
-module.exports.start = function (config, logger) {
+module.exports.start = function (config, logger, callback) {
+  if ( typeof callback !== 'function' ) callback = exit;
 
   // Necessary setup for cloud storage module
   cloudStorage.setProjectName(config.get('googleProjectId'));
   cloudStorage.setServiceAccount(config.get('googleServiceAccount'));
 
-  var backupTs = Date.now();
   var self = this;
 
-  var firebaseUrl = config.get('firebase') || '';
-  this.root = new firebase('https://' + firebaseUrl +  '.firebaseio.com');
+  var firebase = Firebase( config() )
+  this.root = firebase.database()
 
-  // Auth against firebase
-  self.root.auth(config.get('firebaseSecret'), function(err) {
+  var options = {
+    backupBucket: config.get( 'backupBucket' ),
+    backupTimestamp: Date.now(),
+    firebaseName: config.get( 'firebase' ),
+    removeBackup: { timestamp: false, id: false },
+  }
+
+  async.series( [
+    getCloudStorageToken,    // adds { cloudStorageToken } to options
+    getFirebaseToken,        // adds { firebaseToken } to options
+    getUploadUrl,            // adds { uploadUrl } to options
+    createBackup,
+    storeBackupTimestampReference,
+    checkRemoveOldestBackup, // adds { removeBackupOfTimestamp? } to options
+    removeBackupId,
+    removeBackupTimestamp,
+  ], callback )
+
+  function getCloudStorageToken ( next ) {
     // We force ourself to get the token first, because we will use it to bypass
     // our cloud storage module, this request is very special so we build it manually
-    cloudStorage.getToken(function(token) {
-      // This is the upload reuqest, because the file can be so large we use the resumable
-      // upload API of cloud storage. So first we request a url to upload to.
-      request({
-        url: 'https://www.googleapis.com/upload/storage/v1/b/' + config.get('backupBucket') + '/o',
-        qs: { uploadType: 'resumable', 'access_token' : token },
-        method: 'POST',
-        headers: {
-          'X-Upload-Content-Type' : 'application/json',
-          'Content-Type' : 'application/json; charset=UTF-8',
-        },
-        body: JSON.stringify({
-          name: 'backup-' + backupTs,
-          cacheControl: "no-cache"
-        }) 
-      }, function(err, res, body) {
-        var url = res.headers.location;
+    cloudStorage.getToken( function ( token ) {
+      options.cloudStorageToken = token;
+      next()
+    } )
+  }
 
-        // The location returned by google is the url to send the file to for upload
-        // We create a get request to download the data from firebase and pipe it into
-        // a PUT request to googles cloud url, for effeciency.
-        request.get('https://' + config.get('firebase') + '.firebaseio.com/.json?auth=' + config.get('firebaseSecret') + '&format=export').pipe(
-          request.put(url, function(err, res, body) {
-            // We update the list of backups in firebase
-            self.root.child('management/backups/').push(backupTs, function() {
-              // Do cleanup of old backups here, delete ones past 30 days ago
-              self.root.child('management/backups/').once('value', function(snap) {
-                var data = snap.val();
+  function getFirebaseToken ( next ) {
+    firebase.token( 'rm-wh-backup', function ( error, token ) {
+      if ( error ) return next( error )
+      options.firebaseToken = token;
+      next()
+    } )
+  }
 
-                var ids = _.keys(data);
+  function getUploadUrl ( next ) {
+    request( {
+      url: 'https://www.googleapis.com/upload/storage/v1/b/' + options.backupBucket + '/o',
+      qs: { uploadType: 'resumable', 'access_token' : options.cloudStorageToken },
+      method: 'POST',
+      headers: {
+        'X-Upload-Content-Type' : 'application/json',
+        'Content-Type' : 'application/json; charset=UTF-8',
+      },
+      body: JSON.stringify( {
+        name: 'backup-' + options.backupTimestamp,
+        cacheControl: "no-cache"
+      } )
+    }, function onComplete ( error, response, body ) {
+      options.uploadUrl = response.headers.location;
+      next()
+    } )
+  }
 
-                if(ids.length > 30) {
-                  var oldestId = ids[0];
-                  var oldestTimestamp = data[oldestId];
+  function createBackup ( next ) {
+    var url = `https://${ options.firebaseName }.firebaseio.com/.json?access_token=${ options.firebaseToken }&format=export`
+    console.log( 'url:', url )
+    request.get( url ).pipe( request.put( options.uploadUrl, next ) )
+  }
 
-                  self.root.child('management/backups/' + oldestId).remove(function() {
-                    cloudStorage.objects.del(config.get('backupBucket'), 'backup-' + oldestTimestamp, function() {
-                      console.log('Done');
-                      process.exit(0);
-                    });
-                  });
-                } else {
-                  console.log('Done');
-                  process.exit(0);
-                }
-              });
-            });
-          })
-        );
-      });
-    });
+  function storeBackupTimestampReference ( next ) {
+    self.root.ref( 'management/backups/' ).push( options.backupTimestamp, next)
+  }
 
-  });
+  function checkRemoveOldestBackup ( next ) {
+    self.root.ref( 'management/backups/' ).once( 'value', function( snapshot ) {
+      var data = snapshot.val()
+
+      var ids = _.keys( data )
+
+      if(ids.length > 30) {
+        var oldestId = ids[ 0 ]
+        var oldestTimestamp = data[ oldestId ]
+
+        options.removeBackup.id = oldestId;
+        options.removeBackup.timestamp = oldestTimestamp;
+      }
+
+      next()
+    } )
+  }
+
+  function removeBackupId ( next ) {
+    if ( options.removeBackup.id === false ) return next()
+    self.root.ref( 'management/backups/' + options.removeBackup.id ).remove( next )
+  }
+
+  function removeBackupTimestamp ( next ) {
+    if ( options.removeBackup.timestamp === false ) return next()
+    cloudStorage.objects.del( options.backupBucket, 'backup-' + options.removeBackup.timestamp, next )
+  }
+
+  
+  // cloudStorage.getToken(function(token) {
+
+  //   // This is the upload reuqest, because the file can be so large we use the resumable
+  //   // upload API of cloud storage. So first we request a url to upload to.
+  //   request({
+  //     url: 'https://www.googleapis.com/upload/storage/v1/b/' + options.backupBucket + '/o',
+  //     qs: { uploadType: 'resumable', 'access_token' : token },
+  //     method: 'POST',
+  //     headers: {
+  //       'X-Upload-Content-Type' : 'application/json',
+  //       'Content-Type' : 'application/json; charset=UTF-8',
+  //     },
+  //     body: JSON.stringify({
+  //       name: 'backup-' + backupTs,
+  //       cacheControl: "no-cache"
+  //     }) 
+  //   }, function(err, res, body) {
+  //     var url = res.headers.location;
+
+  //     // The location returned by google is the url to send the file to for upload
+  //     // We create a get request to download the data from firebase and pipe it into
+  //     // a PUT request to googles cloud url, for effeciency.
+  //     request.get('https://' + config.get('firebase') + '.firebaseio.com/.json?auth=' + config.get('firebaseSecret') + '&format=export').pipe(
+  //       request.put(url, function(err, res, body) {
+  //         // We update the list of backups in firebase
+  //         self.root.child('management/backups/').push(backupTs, function() {
+  //           // Do cleanup of old backups here, delete ones past 30 days ago
+  //           self.root.child('management/backups/').once('value', function(snap) {
+  //             var data = snap.val();
+
+  //             var ids = _.keys(data);
+
+  //             if(ids.length > 30) {
+  //               var oldestId = ids[0];
+  //               var oldestTimestamp = data[oldestId];
+
+  //               self.root.child('management/backups/' + oldestId).remove(function() {
+  //                 cloudStorage.objects.del(config.get('backupBucket'), 'backup-' + oldestTimestamp, function() {
+  //                   console.log('Done');
+  //                   process.exit(0);
+  //                 });
+  //               });
+  //             } else {
+  //               console.log('Done');
+  //               process.exit(0);
+  //             }
+  //           });
+  //         });
+  //       })
+  //     );
+  //   });
+  // });
+
 };
 
+function exit ( error ) {
+  var exitCode = 0
+  if ( error ) exitCode = 1
+  process.exit( exitCode )
+}
---
+++ test/backup.js
@@ -0,0 +1,14 @@
+var test = require( 'tape' )
+var grunt = require( 'grunt' )
+var backup = require( '../libs/backup.js' )
+var webhookTasks = require( '../Gruntfile.js' )
+
+webhookTasks( grunt )
+
+test( 'backup', function ( t ) {
+  t.plan( 1 )
+  
+  backup.start( grunt.config, console.log, function ( error ) {
+    t.assert( error === undefined, 'Backup completed without error.' )
+  } )
+} )
