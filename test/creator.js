a7fb249ff57330ccf8dbadc22da65d484e674468
rubonics <rubenrodriguez+github@gmail.com>
2018-10-05 15:02:47 -0400

    adds creator tests

--- test/creator.js
+++ test/creator.js
@@ -1,80 +1,109 @@
 var testOptions = require( './env-options.js' )()
 var test = require( 'tape' )
+var async = require( 'async' )
 var grunt = require( 'grunt' )
-var creator = require( '../libs/creator.js' )
-var Firebase = require( '../libs/firebase/index.js' )
 var webhookTasks = require( '../Gruntfile.js' )
 
-var firebaseEscape = require( '../libs/utils/firebase-escape.js' )
-
 webhookTasks( grunt )
+
+var creator = require( '../libs/creator.js' )
+var Firebase = require( '../libs/firebase/index.js' )
+var firebaseEscape = require( '../libs/utils/firebase-escape.js' )
 
 grunt.config.merge( { suppressJobQueue: true } )
 
 var firebase = Firebase( grunt.config.get( 'firebase' ) )
 
-// test( 'add-owner', function ( t ) {
-//   /* Add owner is a task that is accomplished by the `wh create` command
-//      in anticipation of submitting the signal command to create the site. */
-//   t.plan( 1 )
-
-//   var ownerData = {}
-//   ownerData[ firebaseEscape( testOptions.createUserId ) ] = testOptions.createUserId
-
-//   firebase.database()
-//     .ref( `management/sites/${ testOptions.createSiteName }/owners` )
-//     .set( ownerData, onOwnerAdded )
-
-//   function  onOwnerAdded ( error ) {
-//     t.assert( error === null, 'Added site owner without error.' )
-//   }
-// } ) 
-
-// test( 'create-site', function ( t ) {
-//   t.plan( 1 )
-
-//   var command = {
-//     identifier: testOptions.createSiteName,
-//     payload: {
-//       userid: testOptions.createUserId,
-//       sitename: testOptions.createSiteName,
-//     }
-//   }
-
-//   var mockClient = { put: function () {} }
-
-//   var create = creator.start( grunt.config, console.log )
-
-//   create( command, command.identifier, command.payload, mockClient, jobCallback )
-  
-//   function jobCallback ( error ) {
-//     t.assert( error === undefined, 'Create completed without error.' )
-//   }
-
-// } )
-
-// test( 'set-bad-data', function ( t ) {
-//   t.plan( 2 )
-
-//   firebase.database().ref( 'new-key' ).set( {}, function ( error ) {
-//     console.log( 'callback-error' )
-//     console.log( error )
-//     t.assert( error, 'Firebase throws error when setting a key that should not be set.' )
-//   } )
-
-//   firebase.database().ref( 'new-key' ).set( {} )
-//     .then( function () {
-//       t.fail( 'Promise success should not be reached.' )
-//     } )
-//     .catch( function ( error ) {
-//       console.log( 'promise-error' )
-//       console.log( 'error' )
-//       t.asssert(  error, 'Firebase throws error when setting a bad key' )
-//     } )
-// } )
-
-test( 'check-version', function ( t ) {
+test( 'add-owner', function ( t ) {
+  /* Add owner is a task that is accomplished by the `wh create` command
+     in anticipation of submitting the signal command to create the site. */
   t.plan( 1 )
-  console.log( firebase.database().SDK_VERSION )
-  t.assert( firebase.SDK_VERSION !== firebase.database().SDK_VERSION, 'Database & admin version should be different.' )
+
+  var ownerData = {}
+  ownerData[ firebaseEscape( testOptions.createUserId ) ] = testOptions.createUserId
+
+  firebase.database()
+    .ref( `${ firebaseSitePaths( testOptions.createSiteName ).management }/owners` )
+    .set( ownerData, onOwnerAdded )
+
+  function  onOwnerAdded ( error ) {
+    t.assert( error === null, 'Added site owner without error.' )
+  }
+} ) 
+
+
+test( 'create-site', function ( t ) {
+  t.plan( 1 )
+
+  
+  makeCreateWithHandler( createSiteHandler )
+  
+  
+  function createSiteHandler ( error ) {
+    t.assert( error === undefined, 'Create completed without error.' )
+  }
+
 } )
+
+test( 'create-existing-site', function ( t ) {
+  t.plan( 2 )
+
+  makeCreateWithHandler( createExistingSiteHandler )
+
+  function createExistingSiteHandler ( error ) {
+    t.assert( error, 'Create existing site correctly errored.' )
+    t.equal( error.message, 'site-exists', 'Create existing site errored with the correct message.' )
+  }
+
+} )
+
+test( 'clean-up', function ( t ) {
+  t.plan( 1 )
+
+  deleteSite( testOptions.createSiteName, deleteHandler )
+
+  function deleteHandler ( error ) {
+    t.assert( ! error, 'Deleted all firebase site paths without error.' )
+  }
+} )
+
+test.onFinish( process.exit )
+
+function makeCreateWithHandler ( createHandler ) {
+  var createOptions = {
+    identifier: testOptions.createSiteName,
+    payload: {
+      userid: testOptions.createUserId,
+      sitename: testOptions.createSiteName,
+    }
+  }
+
+  var mockClient = { put: function () {} }
+
+  var create = creator.start( grunt.config, console.log )
+
+  return create( createOptions, createOptions.identifier, createOptions.payload, mockClient, createHandler )
+}
+
+function deleteSite ( siteName, callback ) {
+
+  async.parallel( removePaths( firebaseSitePaths( siteName ) ), callback )
+
+  function removePaths ( pathsObject ) {
+    return Object.keys( pathsObject ).map( pathKeyToRemovePathTask )
+
+    function pathKeyToRemovePathTask ( pathKey ) {
+      return function removePathTask ( taskComplete ) {
+        firebase.database().ref( pathsObject[ pathKey ] ).remove( taskComplete )
+      }
+    }
+  }
+}
+
+function firebaseSitePaths ( siteName ) {
+  return {
+    management: `management/sites/${ siteName }`,
+    billing: `billing/sites/${ siteName }`,
+    buckets: `buckets/${ siteName }`,
+  }
+}
