dc8c9a2411bcaab70619086b7e00538c0d4f7d52
rubonics <rubenrodriguez+github@gmail.com>
2018-10-17 14:05:52 -0400

    adds cloudStorage test for uploading a compressed file

---
+++ test/cloudStorage.js
@@ -0,0 +1,23 @@
+var testOptions = require( './env-options.js' )()
+var webhookTasks = require( '../Gruntfile.js' )
+var grunt = require( 'grunt' )
+var test = require( 'tape' )
+
+webhookTasks( grunt )
+
+var cloudStorage = require( '../libs/cloudStorage.js' )
+
+test( 'upload-compressed-object', function ( t ) {
+  t.plan( 3 )
+
+  var uploadOptions = {
+    bucket: grunt.config.get( 'uploadsBucket' ),
+    local: 'file content',
+    remote: 'test/cloud-strorage-upload.txt',
+  }
+  cloudStorage.objects.uploadCompressed( uploadOptions, function ( error, results ) {
+    t.assert( ! error, 'uploaded file content without error.' )
+    t.assert( uploadOptions.bucket === results.bucket, 'uploaded file to the correct bucket.' )
+    t.assert( uploadOptions.remote === results.name, 'uploaded file to the correct path.' )
+  } )
+} )
