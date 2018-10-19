68794967280ed16b9fa992077c2b5014c4b6e914
rubonics <rubenrodriguez+github@gmail.com>
2018-10-19 12:35:46 -0400

    updates siteIndex firebase usage
    + adds test to support ensuring changes to siteIndex's firebase usage is working
    + updates webhook-site-index dependency to a version that aligns with newer firebase interface

---
+++ test/site-index.js
@@ -0,0 +1,32 @@
+var testOptions = require( './env-options.js' )()
+var test = require( 'tape' )
+var grunt = require( 'grunt' )
+var webhookTasks = require( '../Gruntfile.js' )
+
+webhookTasks( grunt )
+
+var siteIndex = require( '../libs/siteIndex.js' )
+
+test( 'index-site-with-no-data', function ( t ) {
+  t.plan( 1 )
+  
+  var indexer = siteIndex.start( grunt.config, console.log )
+
+  var mockClient = { put: function () {} }
+
+  var indexOptions = {
+    identifier: testOptions.siteIndexSiteName,
+    payload: {
+      userid: testOptions.siteIndexUserId,
+      sitename: testOptions.siteIndexSiteName,
+    }
+  }
+
+  indexer( indexOptions, indexOptions.identifier, indexOptions.payload, mockClient, handleSiteIndex )
+
+  function handleSiteIndex ( error ) {
+    t.assert( error, 'Site index completes with error.' )
+  }
+} )
+
+test.onFinish( process.exit )
