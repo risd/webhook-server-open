55b5ba7138235e40e0eb7f530479233d2744cd20
rubonics <rubenrodriguez+github@gmail.com>
2018-11-30 20:14:16 -0500

    [test] updates firebase test to verify how cases such as deleting non-existent nodes are handled

--- test/firebase/admin.js
+++ test/firebase/admin.js
@@ -21,8 +21,9 @@ test( 'firebase-admin', function ( t ) {
   var siteKeyPath = `/management/sites/${ testOptions.firebaseAdminSiteName }/key`
   try {
     db.child( siteKeyPath )
+    t.fail( 'Should not be able to call `child` on the root database object.' )
   } catch ( error ) {
-    t.assert( error, 'Deprecated: Child must be executed on a database reference, not the root database object.' )
+    t.pass( 'Deprecated: Child must be executed on a database reference, not the root database object.' )
   }
 
   var siteKeyPathRef = db.ref( siteKeyPath )
@@ -30,20 +31,32 @@ test( 'firebase-admin', function ( t ) {
 
   siteKeyPathRef.once( 'value', function ( siteKeySnapshot ) {
     var siteKey = siteKeySnapshot.val()
-    t.assert( typeof siteKey === 'string', 'The site key value is a string.' )
+    t.assert( typeof siteKey === 'string' || siteKey === null, 'The site key value is a string.' )
     t.assert( siteKeySnapshot.key === 'key', 'The key of the snapshot is the last part of the path to get the snapshot.' )
     try {
       var keyValueforSnapshot = siteKeySnapshot.name()
     } catch ( error ) {
-      t.assert( error, 'Deprecated: `name` is no longer a function to access the key.' )
+      t.pass( 'Deprecated: `name` is no longer a function to access the key.' )
     }
 
     try {
       var keyValueforSnapshot = siteKeySnapshot.key()
     } catch ( error ) {
-      t.assert( error, 'Deprecated: `key` is no longer a function, instead, a property getter.' )
+      t.pass( 'Deprecated: `key` is no longer a function, instead, a property getter.' )
     }
   } )
+
+  db.ref( '/buckets/site-does-not-exist' ).set( null )
+    .then( handleNonExistentKeySetSuccess )
+    .catch( handleNonExistentKeySetError )
+
+  function handleNonExistentKeySetSuccess () {
+    t.pass( 'Setting a non-existent key path to null completes successfully.' )
+  }
+
+  function handleNonExistentKeySetError () {
+    t.fail( 'Setting a non-existent key path to null errors.' )
+  }
 } )
 
 test.onFinish( process.exit )
