a4d42a5c026849a36191cc2dea9c0c530dfeee07
rubonics <rubenrodriguez+github@gmail.com>
2018-12-01 17:52:05 -0500

    [test] updates create-cname test to use the development domain for the environment configuration, and then delete itself.

--- test/libs/create-cname.js
+++ test/libs/create-cname.js
@@ -1,7 +1,7 @@
 var test = require( 'tape' )
-
 var grunt = require( 'grunt' );
 var webhookTasks = require( '../../Gruntfile.js' );
+var Cloudflare = require( '../../libs/cloudflare/index.js' )
 
 webhookTasks( grunt );
 
@@ -9,22 +9,34 @@ var createCnameRecord = require( '../../libs/creator.js' ).createCnameRecord;
 
 var cloudflareOptions = grunt.config.get( 'cloudflare' )
 var cnameRecordOptions = {
-  record: 'cname-create.risd.systems',
-  content: 'c.storage.googleapis.com',
+  siteBucket: `cname-create.${ grunt.config().developmentDomain }`,
+  usesFastly: false,
 }
+var cloudflare = Cloudflare( cloudflareOptions )
 
 var createCnameRecordOptions = Object.assign( cloudflareOptions, cnameRecordOptions )
 
-test( 'create-dns-record', function ( t ) {
-  t.plan( 1 )
+test( 'create-cname-record', function ( t ) {
+  t.plan( 2 )
 
   var DNSRecord = require( 'cloudflare' ).DNSRecord
 
   createCnameRecord( createCnameRecordOptions, function ( error, cname ) {
     if ( error ) console.log( error )
     t.ok( DNSRecord.is( cname ), 'Return value is DNSRecord' )
+
+    cloudflare.deleteCname( cname )
+      .then( handleDelete )
+      .catch( handleDeleteError )
   } )
 
+  function handleDelete ( cnameStub ) {
+    t.pass( `Deleted the just created CNAME with ID: ${ cnameStub.id }` )
+  }
+ 
+  function handleDeleteError ( error ) {
+    t.fail( `Error during delete of CNAME: ${ error.message }` )
+  }
 } )
 
 test.onFinish( process.exit )
