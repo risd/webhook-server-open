a060b36a294160953515143770e6d6e18e329151
rubonics <rubenrodriguez+github@gmail.com>
2019-02-12 14:47:39 -0500

    [test] updates elastic search tests
    + updates the elastic search tests to include a delay, which allows the tests to complete successfully, since we are communicating with a distributed system which introduces race conditions for each test that relies on the previous test to continue to succeed

--- test/elastic/index.js
+++ test/elastic/index.js
@@ -7,172 +7,202 @@ var WebhookElasticSearchQuery = require( '../../libs/elastic-search/index.js' )
 webhookTasks( grunt )
 
 var siteName = 'test.risd.systems'
+var testDelay = 5000;
 
 var elasticOptions = grunt.config().elastic;
 
 var elasticManager = WebHookElasticSearchManager( elasticOptions )
 var elasticQuery = WebhookElasticSearchQuery( elasticOptions )
 
-// test( 'site-entries', function ( t ) {
-//   t.plan( 1 )
-//   getSiteIndex( siteName )
-//     .then( function ( siteIndex ) {
-//       console.log( siteIndex )
-//       t.assert( Array.isArray( siteIndex ), 'Site index is an array of entries' )
-//     } )
-//     .catch( function ( error ) {
-//       console.log( error )
-//       t.fail( 'Failed to retrieve site index' )
-//     } )
-// } )
+// run tests with the understanding that these functions are communicating
+// with distributed clusters, which might not all be representing the same
+// data immediately
 
-// test( 'populate-index', function ( t ) {
-//   t.plan( 1 )
-//   setIndex( { siteName: siteName, siteData: fullData() } )
-//     .then( function getSitesIndex ( results ) { return getSiteIndex( siteName ) } )
-//     .then( function assertSiteIndex( siteIndex ) {
-//       console.log( siteIndex )
-//       t.assert( siteIndex.length > 0, 'Site index is populated' )
-//     } )
-//     .catch( function catchSiteIndexError ( error ) {
-//       console.log( error )
-//       t.fail( 'Failed to populate index.' )
-//     } )
-// } )
+test( 'index-item', function ( t ) {
+  t.plan( 1 )
 
-// test( 'index-item', function ( t ) {
-//   t.plan( 1 )
+  setTimeout( indexItemTest, testDelay )
 
-//   elasticQuery.index( {
-//     siteName: siteName,
-//     typeName: 'teahouses',
-//     id: Object.keys( fullData().data.teahouses )[ 0 ],
-//     doc: JSON.stringify( fullData().data.teahouses[ Object.keys( fullData().data.teahouses )[ 0 ] ] ),
-//     oneOff: false,
-//   } )
-//     .then( function ( results ) {
-//       t.ok( true, 'Successfully got search results for a content type.' )
-//     } )
-//     .catch( function ( error ) {
-//       t.fail( error, 'Failed to get search results for a content type.' )
-//     } )
-// } )
+  function indexItemTest () {
+    elasticQuery.index( {
+      siteName: siteName,
+      typeName: 'teas',
+      id: Object.keys( fullData().data.teas )[ 0 ],
+      doc: JSON.stringify( fullData().data.teas[ Object.keys( fullData().data.teas )[ 0 ] ] ),
+      oneOff: false,
+    } )
+      .then( function ( results ) {
+        t.ok( true, 'Successfully got search results for a content type.' )
+      } )
+      .catch( function ( error ) {
+        t.fail( error, 'Failed to get search results for a content type.' )
+      } )
 
-// test( 'site-search', function ( t ) {
-//   t.plan( 1 )
-//   
-//   elasticQuery.search( {
-//     siteName: siteName,
-//     query: 'honey bush',
-//     page: 1,
-//   } )
-//     .then( function ( results ) {
-//       if ( results && results.hits && results.hits.hits ) {
-//         console.log( 'results.hits.hits' )
-//         console.log( results.hits.hits )
-//       }
-//       else if ( results && results.hits ) {
-//         console.log( 'results.hits' )  
-//         console.log( results.hits )  
-//       }
-      
-//       t.ok( 'Successfully got search results without a content type.' )
-//     } )
-//     .catch( function ( error ) {
-//       console.log( error )
-//       if ( error && error.body && error.body.error ) {
-//         console.log( 'error.body.error' )
-//         console.log( error.body.error )
-//       }
-//       t.fail( 'Failed to get search results without a content type.' )
-//     } )
-// } )
+  }
+} )
 
-// test( 'site-search-with-type', function ( t ) {
-//   t.plan( 1 )
-//   elasticQuery.search( {
-//     siteName: siteName,
-//     query: 'vanilla',
-//     typeName: 'teas',
-//     page: 1,
-//   } )
-//     .then( function ( results ) {
-//       if ( results && results.hits && results.hits.hits ) {
-//         console.log( 'results.hits.hits' )
-//         // console.log( results.hits.hits )
-//         // console.log( results.hits.hits.map( (d) => d._source.doc ) )
-//         console.log( results.hits.hits.map( (d) => d._source.doc.name ) )
-//       }
-//       else if ( results && results.hits ) {
-//         console.log( 'results.hits' )  
-//         console.log( results.hits )  
-//       }
-//       t.ok( results.hits.hits.length === 1, 'Successfully got search results for a content type.' )
-//     } )
-//     .catch( function ( error ) {
-//       console.log( error )
-//       t.fail( error, 'Failed to get search results for a content type.' )
-//     } )
-// } )
+test( 'populate-index', function ( t ) {
+  t.plan( 1 )
+  setTimeout( populateIndexTest, testDelay )
 
-// test( 'site-search-with-type-one-hit', function ( t ) {
-//   t.plan( 1 )
-//   elasticQuery.search( {
-//     siteName: siteName,
-//     query: 'providence',
-//     typeName: 'teahouses',
-//     page: 1,
-//   } )
-//     .then( function ( results ) {
-//       t.ok( results.hits.hits.length === 1, 'Successfully got search results for a content type.' )
-//     } )
-//     .catch( function ( error ) {
-//       console.log( error )
-//       t.fail( 'Failed to get search results for a content type.' )
-//     } )
-// } )
+  function populateIndexTest () {
+    setIndex( { siteName: siteName, siteData: fullData() } )
+      .then( function getSitesIndex ( results ) { return getSiteIndex( siteName ) } )
+      .then( function assertSiteIndex( siteIndex ) {
+        // console.log( siteIndex )
+        t.assert( siteIndex.length > 0, 'Site index is populated' )
+      } )
+      .catch( function catchSiteIndexError ( error ) {
+        console.log( error )
+        t.fail( 'Failed to populate index.' )
+      } )
+  }
+} )
 
-// test( 'delete-type', function ( t ) {
-//   t.plan( 1 )
+test( 'site-entries', function ( t ) {
+  t.plan( 1 )
 
-//   elasticQuery.deleteType( {
-//     siteName: siteName,
-//     typeName: 'teas',
-//   } )
-//     .then( function () {
-//       t.ok( true, 'Successfully deleted type.' )
-//     } )
-//     .catch( function ( error ) {
-//       t.fail( error, 'Failed to delete type.' )
-//     } )
-// } )
+  setTimeout( siteEntriesTest, testDelay )
 
+  function siteEntriesTest () {
+    getSiteIndex( siteName )
+      .then( function ( siteIndex ) {
+        // console.log( siteIndex )
+        // console.log( siteIndex.map( ( d ) => d._source.doc ) )
+        t.assert( Array.isArray( siteIndex ), 'Site index is an array of entries' )
+      } )
+      .catch( function ( error ) {
+        console.log( error )
+        t.fail( 'Failed to retrieve site index' )
+      } )
+
+  }
+} )
+
+test( 'site-search', function ( t ) {
+  t.plan( 1 )
+
+  setTimeout( siteSearchTest, testDelay )
+
+  function siteSearchTest () {
+    elasticQuery.search( {
+      siteName: siteName,
+      query: 'worldwide',
+      page: 1,
+    } )
+      .then( function ( results ) {
+        t.ok( Array.isArray( results ), 'Successfully got search results without a content type.' )
+      } )
+      .catch( function ( error ) {
+        t.fail( error, 'Failed to get search results without a content type.' )
+      } )
+  }
+} )
+
+test( 'site-search-with-type', function ( t ) {
+  t.plan( 1 )
+
+  setTimeout( siteSearchWithTypeTest, testDelay )
+
+  function siteSearchWithTypeTest () {
+    elasticQuery.search( {
+      siteName: siteName,
+      query: 'grad',
+      typeName: 'gradshow',
+      page: 1,
+    } )
+      .then( function ( results ) {
+        t.ok( results.length === 1, 'Successfully got search results for a content type.' )
+      } )
+      .catch( function ( error ) {
+        console.log( error )
+        t.fail( error, 'Failed to get search results for a content type.' )
+      } )
+
+  }
+} )
+
+test( 'site-search-with-type-one-hit', function ( t ) {
+  t.plan( 1 )
+
+  setTimeout( siteSearchWithTypeOneHitTest, testDelay )
+
+  function siteSearchWithTypeOneHitTest () {
+    elasticQuery.search( {
+      siteName: siteName,
+      query: 'providence',
+      typeName: 'teahouses',
+      page: 1,
+    } )
+      .then( function ( results ) {
+        t.ok( results.length === 1, 'Successfully got search results for a content type.' )
+      } )
+      .catch( function ( error ) {
+        console.log( error )
+        t.fail( 'Failed to get search results for a content type.' )
+      } )
+  }
+} )
+
+test( 'delete-type', function ( t ) {
+  t.plan( 1 )
+
+  setTimeout( deleteTypeTest, testDelay )
+
+  function deleteTypeTest () {
+    elasticQuery.deleteType( {
+      siteName: siteName,
+      typeName: 'teahouses',
+    } )
+      .then( function () {
+        t.ok( true, 'Successfully deleted type.' )
+      } )
+      .catch( function ( error ) {
+        t.fail( error, 'Failed to delete type.' )
+      } )
+
+  }
+} )
+
+// this test is redundant of the `delete-site-index` test
 // test( 'empty-index', function ( t ) {
 //   t.plan( 1 )
-//   setIndex( { siteName: siteName, siteData: emptyData() } )
-//     .then( function ( results ) {
-//       return getSiteIndex( siteName )
-//     } )
-//     .then( function ( siteIndex ) {
-//       t.assert( siteIndex.length === 0, 'Empty array for siteIndex.' )
-//     } )
-//     .catch( function ( error ) {
-//       t.fail( 'Failed to empty index.' )
-//     } )
+
+//   setTimeout( emptyIndexTest, testDelay )
+
+//   function emptyIndexTest () {
+//     setIndex( { siteName: siteName, siteData: emptyData() } )
+//       .then( function ( results ) {
+//         return getSiteIndex( siteName )
+//       } )
+//       .then( function ( siteIndex ) {
+//         t.assert( siteIndex.length === 0, 'Empty array for siteIndex.' )
+//       } )
+//       .catch( function ( error ) {
+//         t.fail( 'Failed to empty index.' )
+//       } )
+//   }
 // } )
 
-// test( 'delete-site-index', function ( t ) {
-//   t.plan( 1 )
+test( 'delete-site-index', function ( t ) {
+  t.plan( 1 )
 
-//   elasticQuery.deleteSite( { siteName: siteName } )
-//     .then( function () {
-//       t.ok( true, 'Successfully deleted site index.' )
-//     } )
-//     .catch( function ( error ) {
-//       console.log( error )
-//       t.fail( error, 'Failed to delete site index.' )
-//     } )
-// } )
+  setTimeout( deleteSiteIndexTest, testDelay )
+
+  function deleteSiteIndexTest () {
+    elasticQuery.deleteSite( { siteName: siteName } )
+      .then( function () {
+        t.ok( true, 'Successfully deleted site index.' )
+      } )
+      .catch( function ( error ) {
+        console.log( error )
+        t.fail( error, 'Failed to delete site index.' )
+      } )
+  }
+} )
+
+
+// --- helper functions ---
 
 function setIndex ( options ) {
   var siteName = options.siteName;
@@ -249,7 +279,8 @@ function fullData () {
           "last_updated": "2019-01-30T18:29:00-05:00",
           "name": "vanilla honey bush",
           "preview_url": "29a2b093-730e-fb4d-ff08-825e005c07d4",
-          "publish_date": "2019-01-30T18:29:00-05:00"
+          "publish_date": "2019-01-30T18:29:00-05:00",
+          "availability": "in the majority of teahouses worldwide."
         },
         "-LXW56589LNe8ScrCXAV": {
           "_sort_create_date": 1548890940,
@@ -259,7 +290,8 @@ function fullData () {
           "last_updated": "2019-01-30T18:29:00-05:00",
           "name": "silver tips white tea",
           "preview_url": "dccd4614-eec7-1db0-ad26-f1b9372d6749",
-          "publish_date": "2019-01-30T18:29:00-05:00"
+          "publish_date": "2019-01-30T18:29:00-05:00",
+          "availability": "available in the northern reaches of the Northern Americas"
         }
       }
     },
