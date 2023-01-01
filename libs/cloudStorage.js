743300906e09bf409c7644e77a6c47e530736a96
rubonics <rubenrodriguez+github@gmail.com>
2023-01-01 15:38:25 +0100

    removes unused cloudstorage api's, and removes the usage of the rest api in favor of the google-cloud/storage npm module

--- libs/cloudStorage.js
+++ libs/cloudStorage.js
@@ -61,120 +61,6 @@ if (keyFilename && projectName && googleServiceAccount) {
 }
 
 
-// TODO: remove this and its use throughout the module
-/*
-* Run a json request against google cloud stoage, handles getting token
-*
-* @param options  Object of options to pass to the json request, mostly same options passed to request module
-* @param callback Callback to call when finished
-*/
-function jsonRequest(options, callback)  {
-  console.log('cloud-storage:json-request')
-  if(!options.qs)
-  {
-    options.qs = {};
-  }
-
-  options.qs.access_token = oauthToken;
-
-  var multiData = [];
-
-  if(options.multipart)
-  {
-    var index = 0;
-    options.multipart.forEach(function(multi) {
-      multiData.push({ index: index, body: multi.body});
-      index = index + 1;
-    });
-  }
-
-  let formData
-  if (typeof options.formData === 'object' && options.formData !== null) {
-    formData = new FormData()
-    Object.keys(options.formData).forEach((key) => {
-      formData.append(key, options.formData[key])
-    })
-  }
-  
-  var reqOptions = {
-    url: options.url,
-    params: options.qs,
-    method: options.method,
-    // json: options.multipart ? null : (options.data || true),
-    headers: options.headers || null,
-    data: options.form || formData || options.data || null,
-  };
-
-  if(options.binary) {
-    reqOptions['encoding'] = null;
-  }
-
-  // If the request wants to have a stream back ignore token, the caller is
-  // responsible for making sure a token is active
-  if(options.stream) {
-    return axios(reqOptions);
-  } else {
-    console.log('cloud-storage:json-request:make-request')
-    console.log(reqOptions)
-    axios(reqOptions)
-      .then((res) => {
-        console.log('cloud-storage:json-request:response')
-        if (!res) return callback(500, null)
-        else if (res.statusCode/100 === 2) {
-          callback(null, body);
-        }
-        else if (res.statusCode === 401) {
-          refreshToken(function( error ) {
-            if ( error ) return callback( error )
-            if(options.multipart) {
-              multiData.forEach(function(item) {
-                options.multipart[item.index].body = item.body;
-              });
-            }
-
-            jsonRequest(options, callback);
-          });
-        }
-        else {
-          callback(res.statusCode, null);
-        }
-      })
-      .catch((error) => {
-        console.log('cloud-storage:json-request:error')
-        if (error.message.indexOf(400) > -1) {
-          console.log(error.message)
-          refreshToken((error) => {
-            if (error) return callback(error)
-            return jsonRequest(options, callback)
-          })
-        }
-        else callback(error)
-      })
-  }
-}
-
-// Sets the google project name we authenticate against
-// module.exports.setProjectName = function(project) {
-//   projectName = project;
-// }
-
-// // Sets the google service account email with authenticate with
-// module.exports.setServiceAccount = function(account) {
-//   googleServiceAccount = account;
-// }
-
-// Manually get token, used when wanting a stream back, caller is
-// responsible for making sure token is valid
-module.exports.getToken = function(callback) {
-  refreshToken(callback);
-};
-
-// Init, manually refreshes the token before we do any requests
-// just to get things started
-module.exports.init = function(callback) {
-  refreshToken(callback);
-}
-
 // Sets the key file file, not curretly used
 module.exports.setKeyFile = function(file) {
   keyFile = file;
@@ -199,17 +85,6 @@ var bucketsAPI = {
       })
     if (typeof callback === 'function') chainCallbackResponse(chain, callback)
     else return chain
-  },
-
-  // List all buckets in the project
-  list: function(callback) {
-    jsonRequest({
-      url: 'https://www.googleapis.com/storage/v1/b',
-      qs: {
-        'project' : projectName
-      },
-      method: 'GET',
-    }, callback)
   },
 
   // Create a new bucket, makes the bucket a website hosting bucket
@@ -315,56 +190,6 @@ var objectsAPI = {
       callback( null, completeList )
     }
 
-  },
-
-  // list webhook-uploads
-  listUploads: function(bucket, options, callback) {
-    if ( typeof options === 'function' ) callback = options;
-    if ( !options ) options = {};
-    
-    var qs = Object.assign( {
-      fields: 'kind,items(name,md5Hash),nextPageToken',
-      prefix: 'webhook-uploads/',
-    }, options )
-    
-    jsonRequest({
-      url: 'https://www.googleapis.com/storage/v1/b/' + bucket + '/o',
-      qs: qs
-    }, callback);
-  },
-
-  // List all objects with more information (md5hash, updated time)
-  listMore: function(bucket, callback) {
-    jsonRequest({
-      url: 'https://www.googleapis.com/storage/v1/b/' + bucket + '/o',
-      qs: { fields: 'kind,items(name,md5Hash,updated)', delimiter: 'webhook-uploads/' }
-    }, callback);
-  },
-
-  // Copy an object
-  copy: function ( sourceBucket, sourceFile, destinationBucket, destinationFile, callback ) {
-    jsonRequest({
-      url: 'https://www.googleapis.com/storage/v1/b/' + sourceBucket + '/o/' + sourceFile + '/copyTo/b/' + destinationBucket + '/o/' + destinationFile,
-      method: 'POST',
-    }, callback)
-  },
-
-  // Rewrite an object
-  rewrite: function ( sourceBucket, sourceFile, destinationBucket, destinationFile, callback ) {
-    jsonRequest({
-      url: 'https://www.googleapis.com/storage/v1/b/' + sourceBucket + '/o/' + sourceFile + '/rewriteTo/b/' + destinationBucket + '/o/' + destinationFile,
-      method: 'POST',
-    }, callback)
-  },
-
-  // Get an object from a bucket, return stream for caller to manipulate
-  getStream: function(bucket, file) {
-    return jsonRequest({
-      url: 'https://www.googleapis.com/storage/v1/b/' + bucket + '/o/' + file,
-      qs: { alt: 'media' },
-      binary: true,
-      stream: true
-    });
   },
 
   // Get an object from a bucket
