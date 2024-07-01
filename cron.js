2f70c5b5f998569e8dc0c32e44cc153ba84b3c85
rubonics <rubenrodriguez+github@gmail.com>
2024-07-01 12:01:02 -0400

    [cron] migrate to async function for readability an composability

--- cron.js
+++ cron.js
@@ -7,16 +7,16 @@ webhookTasks(grunt)
 
 const job = new CronJob(
   '0 0 0 * * *', // cronTime
-  function () {
-    backup.start(grunt.config)
-      .then(({ file, timestamp }) => {
-        console.log('backup-complete')
-        console.log({ file, timestamp })
-      })
-      .catch((error) => {
-        console.log('bacup-error')
-        console.log(error)
-      })
+  async function () {
+    try {
+      const { file, timestamp } = await backup.start(grunt.config)
+      console.log('backup-complete')
+      console.log({ file, timestamp })
+    }
+    catch (error) {
+      console.log('bacup-error')
+      console.log(error)
+    }
   }, // onTick
   null, // onComplete
   true, // start
