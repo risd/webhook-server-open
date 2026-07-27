7c0da21d80161faf3d84c52b920d17f3aa2adf57
rubonics <rubenrodriguez+github@gmail.com>
2026-07-27 17:43:11 -0400

    libs[builder] add min release age constraint to npm install. was going to put this into all sites, but its a nice constraint to ensure the server doesn't have a supply chain attack attempted.

--- libs/builder.js
+++ libs/builder.js
@@ -114,7 +114,7 @@ function configure (config) {
 
       try {
         console.log('npm-install')
-        await runInDir('npm', ['install', '--cache=nope'], { cwd: buildFolder })  
+        await runInDir('npm', ['install', '--cache=nope', '--min-release-age=1'], { cwd: buildFolder })
       }
       catch (error) {
         console.log(error)
